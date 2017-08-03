e3c5ced785b69e0266b4664efba8f2c1a65e0b93
Natalie Boehm <nmb56@pitt.edu>
2017-08-03 12:46:41 -0400

    add print statements

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,13 @@ fn main() {
         .build()
         .expect("Failed to build message");
 
+    let mailgun_username = &env::var("MAILGUN_USERNAME").unwrap_or("username".to_string())[..];
+    let mailgun_password = &env::var("MAILGUN_PASSWORD").unwarp_or("password".to_string())]..];
+    println!("username: {:?} password: {:?}", mailgun_username, mailgun_password);
     let mut transport = SmtpTransportBuilder::new(("sandboxc2cd6ddb46a044bc8980ce9d77f67c6a.mailgun.org", SUBMISSION_PORT))
         .expect("Failed to create transport")
         .hello_name("sandboxc2cd6ddb46a044bc8980ce9d77f67c6a.mailgun.org")
-        .credentials(&env::var("MAILGUN_USERNAME").unwrap_or("username".to_string())[..], &env::var("MAILGUN_PASSWORD").unwrap_or("password".to_string())[..])
+        .credentials(mailgun_username, mailgun_password)
         .security_level(SecurityLevel::AlwaysEncrypt)
         .smtp_utf8(true)
         .authentication_mechanism(Mechanism::CramMd5)
