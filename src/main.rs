a603c3233a695da114fdcef468564050b2a3c91e
Natalie Boehm <nmb56@pitt.edu>
2017-08-04 11:33:59 -0400

    delete print statements

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,11 @@ use lettre::transport::smtp::SUBMISSION_PORT;
 use lettre::transport::EmailTransport;
 
 fn main() {
-    println!("Hello, world!");
     dotenv().ok();
 
     let mailgun_username = env::var("MAILGUN_SMTP_LOGIN").unwrap();
     let mailgun_password = env::var("MAILGUN_SMTP_PASSWORD").unwrap();
     let mailgun_server = env::var("MAILGUN_SMTP_SERVER").unwrap();
-    println!("username: {:?} password: {:?} server: {:?}", mailgun_username, mailgun_password, mailgun_server);
-    
-    //let mailgun_username = &env::var("MAILGUN_USERNAME").unwrap_or("username".to_string())[..];
-    //let mailgun_password = &env::var("MAILGUN_PASSWORD").unwrap_or("password".to_string())[..];
 
     let email = EmailBuilder::new()
         .to("natboehm15@gmail.com")
@@ -37,9 +32,6 @@ fn main() {
         .smtp_utf8(true)
         .authentication_mechanism(Mechanism::Plain)
         .build();
-    println!("built, about to send");
     let result = transport.send(email);
-    println!("sent, about to expect");
     let result = result.expect("unable to send email");
-    println!("result: {:?}", result);
 }
